05f34a1dafbdd406670e3d329fbff8d02216aa43
Virendra <virendra.thakur31@gmail.com>
2021-03-20 00:12:48 +0530

    LandingPagemodel added.

---
+++ DemoAppXamarin/DemoAppXamarin/PageModels/LandingPageModel.cs
@@ -0,0 +1,24 @@
+﻿using DemoAppXamarin.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace DemoAppXamarin.PageModels
+{
+    public class LandingPageModel : BasePageModel
+    {
+        public ICommand GetUserDetails { get; set; }
+
+        public LandingPageModel()
+        {
+            GetUserDetails = new Command(OnUserDetailsClicked); 
+        }
+
+        private void OnUserDetailsClicked(object obj)
+        {
+            StaticHelper.InitializeAndShowMasterDetailPage();
+        }
+    }
+}
