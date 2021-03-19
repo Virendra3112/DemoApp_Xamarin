a8df907cbcb689de0357fca870bbb1e50636338e
Virendra <virendra.thakur31@gmail.com>
2021-03-20 00:09:42 +0530

    Web servcies added.

---
+++ DemoAppXamarin/DemoAppXamarin/WebServices/IWebService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace DemoAppXamarin.WebServices
+{
+    public interface IWebService
+    {
+        Task<string> GetData<T>(string url) where T : class, new();
+    }
+}
---
+++ DemoAppXamarin/DemoAppXamarin/WebServices/WebService.cs
@@ -0,0 +1,58 @@
+﻿using DemoAppXamarin.Helpers;
+using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace DemoAppXamarin.WebServices
+{
+    public class WebService : IWebService
+    {
+        static HttpClient httpClient = ApiHelper.GetHttpClient();
+
+        public async Task<string> GetData<T>(string url) where T : class, new()
+        {
+            string apiResult = null;
+            try
+            {
+                //var authHeader = new AuthenticationHeaderValue("bearer", token);
+                //httpClient.DefaultRequestHeaders.Authorization = authHeader;
+
+                string apiRequest = APIEndPoints.ServiceURI + url;
+                Debug.WriteLine($"API Start {url}" + DateTime.Now);
+
+                var response = await httpClient.GetAsync(apiRequest).ConfigureAwait(false);
+
+                Debug.WriteLine($"API END {url}" + DateTime.Now);
+
+                if (HttpStatusCode.OK == response.StatusCode)
+                {
+                    apiResult = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                }
+
+            }
+            catch (JsonReaderException)
+            {
+                return null;
+            }
+            catch (JsonException)
+            {
+                return null;
+            }
+            catch (System.IO.IOException)
+            {
+                return null;
+            }
+            catch (System.Exception)
+            {
+                //throw;
+            }
+
+
+            return apiResult;
+        }
+    }
+}
